fc7526c2d9bce6157518ed5466e9cdbf04fe19c7
AlansoriMZ <alansori2912@gmail.com>
2022-03-05 13:00:08 +0700

    wppb-prak2-percobaan2

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,14 @@ class DetailScreen extends StatelessWidget {
                 ),
               ),
             ),
+            Container(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: const Text(
+                "Museum inside a decomissioned Russian war submarine with tours & an adjacent park with cafes. Clean and well maintained. Car park cost 10k, entrance fee 15k/person. You can see KRI Pasopati there, it is a russian shiskey class. You can also watch the video about the Indonesian Navy at the building beside the submarine.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0,),
+              ),
+            )
           ],
         ),
       ),
